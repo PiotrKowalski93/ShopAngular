8ad6ba23037d92edc552a8ffb4d4c15c9813c0b3
Piotr Kowalski <kowalskii1@op.pl>
2018-03-18 16:14:53 +0100

    ping REST service from angular component, enable CORS on WebApi

--- ShopService/ShopService/Global.asax.cs
+++ ShopService/ShopService/Global.asax.cs
@@ -19,5 +19,22 @@ namespace ShopService
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+            var response = context.Response;
+
+            response.AddHeader("Access-Control-Allow-Origin", "*");
+            response.AddHeader("X-Frame-Options", "ALLOW-FROM *");
+
+            if (context.Request.HttpMethod == "OPTIONS")
+            {
+                response.AddHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT");
+                response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                response.AddHeader("Access-Control-Max-Age", "1000000");
+                response.End();
+            }
+        }
     }
 }
--- ShopService/ShopService/Models/Users.cs
+++ ShopService/ShopService/Models/Users.cs
@@ -12,10 +12,9 @@ namespace ShopService.Models
     using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Users
     {
-        [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public string SecondName { get; set; }
